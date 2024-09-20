cdb8cf6b920b0602ee006e5e0c9f995bfa2a061b
demaao <dema.omar123@gmail.com>
2024-09-20 12:36:42 +0300

    הוסף תיאור של השינוי שעשית

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Purchase.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Purchase.java
@@ -2,10 +2,8 @@ package il.cshaifasweng.OCSFMediatorExample.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 @Entity
-@Table (name = "purchases")
 public class Purchase implements Serializable {
 
     @Id
@@ -13,45 +11,23 @@ public class Purchase implements Serializable {
     private int id;
 
     private String productType;
-    private LocalDateTime purchaseDate;
+    private String purchaseDate;
     private String paymentMethod;
-    private String purchaseDescription;
-    private String branchName;
     private double pricePaid;
-    private int quantity;
-
 
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
 
-    public Purchase() {
-    }
+    public Purchase() {}
 
-    //for cinema movies purchases
-    public Purchase(String productType, LocalDateTime purchaseDate, String paymentMethod, double pricePaid, Customer customer, String branchName, int Quantity, String purchaseDescription) {
+    public Purchase(String productType, String purchaseDate, String paymentMethod, double pricePaid, Customer customer) {
         this.productType = productType;
         this.purchaseDate = purchaseDate;
         this.paymentMethod = paymentMethod;
         this.pricePaid = pricePaid;
         this.customer = customer;
-        this.branchName = branchName;
-        this.quantity = Quantity;
-        this.purchaseDescription = purchaseDescription;
-
-    }
-
-    //for home movies purchases
-    public Purchase(String productType, LocalDateTime purchaseDate, String paymentMethod, double pricePaid, Customer customer, String purchaseDescription) {
-        this.productType = productType;
-        this.purchaseDate = purchaseDate;
-        this.paymentMethod = paymentMethod;
-        this.pricePaid = pricePaid;
-        this.customer = customer;
-        this.purchaseDescription = purchaseDescription;
-        this.quantity = 1;
-
     }
 
     // Getters and setters
@@ -59,7 +35,7 @@ public class Purchase implements Serializable {
         return productType;
     }
 
-    public LocalDateTime getPurchaseDate() {
+    public String getPurchaseDate() {
         return purchaseDate;
     }
 
@@ -78,24 +54,4 @@ public class Purchase implements Serializable {
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
-
-
-    public int getId() {return id;}
-    public void setId(int id) {this.id = id;}
-
-    public String getPurchaseDescription() {return purchaseDescription;}
-    public void setPurchaseDescription(String purchaseDescription) {}
-
-    public String getBranchName() {return branchName;}
-    public void setBranchName(String branchName) {this.branchName = branchName;}
-
-    public int getQuantity() {return quantity;}
-    public void setQuantity(int quantity) {this.quantity = quantity;}
-
-    public void setPurchaseDate(LocalDateTime purchaseDate) {
-        this.purchaseDate = purchaseDate;
-
-    }
 }
-
-
